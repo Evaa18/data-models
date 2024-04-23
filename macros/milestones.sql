0ecb0c931b88174f78dee65d936e596b428dbfd8
Cyril Duchon-Doris <cyril@myjobglasses.com>
2024-04-23 10:57:25 +0200

    fix: clarify ambassador classes and prepare future releases
    - include pre-ambivalence v1 professionals in the ambassador_except_youth category
    - prepare ambivalence v2 youth ambassadors release
    
    Asana: https://app.asana.com/0/0/1207147523565451/f
    Notion: https://www.notion.so/myjobglasses/Ambassador-type-classification-9b1f28ba064c4991825834e4eeb8a40e?pvs=4

---
+++ macros/milestones.sql
@@ -0,0 +1,9 @@
+{%- macro professional_members_release_date() -%}
+{# Alwo known as ambivalence v1 release -#}
+"2023-10-12"
+{%- endmacro -%}
+
+{%- macro youth_ambassadors_release_date() -%}
+{# Arbitrary date before official release -#}
+"2024-05-30"
+{%- endmacro -%}
